f39e78f45b3369df0a767859e2624862a2662e10
ricardo africano <67450338+ricardoafricano92@users.noreply.github.com>
2022-08-07 20:28:32 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,117 @@
+function aplicarnumero7(numero){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[0].innerHTML
+
+
+}
+
+
+function aplicarnumero8(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[1].innerHTML
+
+
+}
+
+function aplicarnumero9(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[2].innerHTML
+
+
+}
+
+function aplicarasterisco(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[3].innerHTML
+
+
+}
+
+function aplicarnumero4(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[4].innerHTML
+
+
+}
+
+function aplicarnumero5(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[5].innerHTML
+
+
+}
+
+
+function aplicarnumero6(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[6].innerHTML
+
+
+}
+function aplicarminus(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[7].innerHTML
+
+
+}
+
+function aplicarnumero1(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[8].innerHTML
+
+
+}
+function aplicarnumero2(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[9].innerHTML
+
+
+}
+function aplicarnumero3(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[10].innerHTML
+
+
+}
+
+function aplicarplus(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[11].innerHTML
+
+
+}
+
+function aplicardiv(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[12].innerHTML
+
+
+}
+
+function aplicarnumero0(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[13].innerHTML
+
+
+}
+function aplicarpunto(){
+    var botones = document.getElementsByClassName("num");
+var barra = document.querySelector("#numbers");
+barra.value=barra.value+ botones[14].innerHTML
+
+
+}
